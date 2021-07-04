7d625e3a910ddbd6883b6b40abc576272e80d151
Matheus Sato <matheusdasilvasato@gmail.com>
2021-07-04 00:10:39 -0300

    Updating Assets

---
+++ LeituRA_Unity/Assets/Scripts/ToggleModel.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleModel : MonoBehaviour
+{
+    public GameObject modelFirst;
+    private bool modelFirstBool;
+    public GameObject modelSecond;
+    private bool modelSecondBool;
+    public ParticleSystem particles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        modelFirst.SetActive(true);
+        modelFirstBool = true;
+        modelSecond.SetActive(false);
+        modelSecondBool = false;
+        if (particles) {
+            particles.Stop();
+        }  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) || (Input.GetKeyDown(KeyCode.Space)))
+        // if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (modelFirstBool == true)
+            {
+                modelFirst.SetActive(false);
+                modelFirstBool = false;
+                modelSecond.SetActive(true);
+                modelSecondBool = true;
+                if (particles) {
+                    particles.Play();
+                }
+            } 
+            else if (modelFirstBool == false) 
+            {  
+                modelFirst.SetActive(true);
+                modelFirstBool = true;
+                modelSecond.SetActive(false);
+                modelSecondBool = false;
+                if (particles) {
+                    particles.Stop();
+                }
+            }
+        } 
+    }
+}
